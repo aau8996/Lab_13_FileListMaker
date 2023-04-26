4eeed6f6e1c0205f532b534c805867b310c68b0a
Andy Au <auad@mail.uc.edu>
2023-04-26 14:23:02 -0400

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,173 @@
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean quit = false;
+        boolean dirty = false;
+        String currentFile = null;
+        ArrayList<String> userList = new ArrayList<String>();
+        do{
+            switch(getUserOption(currentFile)){
+                case "A":
+                    addOption(userList);
+                    dirty = true;
+                    break;
+                case "D":
+                    deleteOption(userList, currentFile);
+                    dirty = true;
+                    break;
+                case "V":
+                    printOption(userList, currentFile);
+                    break;
+                case "C":
+                    clearOption(userList);
+                    dirty = true;
+                    break;
+                case "O":
+                    currentFile = openOption(dirty, userList, currentFile);
+                    dirty = false;
+                    break;
+                case "S":
+                    currentFile = saveOption(userList, currentFile);
+                    dirty = false;
+                    break;
+                case "Q":
+                    dirty = checkForSave(dirty, userList, currentFile);
+                    quit = quitOption(dirty);
+            }
+        }while(!quit);
+    }
+    public static String getUserOption(String currentFile) {
+        if(currentFile != null){
+            System.out.println("Current file: " + currentFile);
+        }
+        System.out.println(" A – Add an item to the list \n D – Delete an item from the list \n V - View the list \n C - Clear the list \n O - Open a list file from disk (Dialog may appear in background) \n S - Save current list to disk \n Q – Quit the program");
+        Scanner scan = new Scanner(System.in);
+        String prompt = "Enter an option (A, D, V, C, O, S, Q)";
+        ArrayList<String> validOptions = new ArrayList<String>(){{
+            add("A");
+            add("D");
+            add("V");
+            add("C");
+            add("O");
+            add("S");
+            add("Q");
+        }};
+        String option = SafeInput.getValidOption(scan, prompt, validOptions);
+
+        System.out.println(option);
+        return option;
+    }
+
+    public static void addOption(ArrayList<String> list){
+        Scanner scan = new Scanner(System.in);
+        String prompt = "Enter the item you want to add";
+        list.add(SafeInput.getNonZeroLenString(scan, prompt));
+    }
+
+    public static void  deleteOption(ArrayList<String> list, String currentFile){
+        printOption(list, currentFile);
+
+        if(list.size() > 0) {
+            Scanner scan = new Scanner(System.in);
+            String prompt = "Enter the integer of the item you want to delete";
+            list.remove(SafeInput.getRangedInt(scan, prompt, 1, list.size()) - 1);
+        }
+    }
+
+    public static void printOption(ArrayList<String> list, String currentFile){
+
+        if(currentFile != null){
+            System.out.println("Current file: " + currentFile);
+        }
+        if(list.size() == 0){
+            System.out.println("The list is empty");
+        }else{
+            for(int i = 0; i < list.size(); i++){
+                System.out.println((i + 1) + ") " + list.get(i));
+            }
+        }
+        System.out.println();
+    }
+
+    public static void clearOption(ArrayList<String> list){
+        list.clear();
+    }
+
+    public static String openOption(boolean dirty, ArrayList<String> list, String currentFile){
+        if(dirty){
+            checkForSave(dirty, list, currentFile);
+        }
+        clearOption(list);
+        Scanner input = null;
+        JFileChooser jfc = new JFileChooser();
+        jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        jfc.showSaveDialog(null); // Makes dialog appear
+        String path = jfc.getSelectedFile().getAbsolutePath(); // Gets path of selected file
+        String fileName = jfc.getSelectedFile().getName(); // Gets name of selected file
+        File inputFile = new File(path);
+        if (inputFile.exists()){ // Makes sure that the file exists before continuing
+            try{
+                input = new Scanner(inputFile);
+                // Defining buffered reader to define the chosen file
+                BufferedReader br = new BufferedReader(new FileReader(inputFile));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    list.add(line);
+                }
+                currentFile = path;
+                input.close();
+            }catch(Exception ex){
+                System.out.println("Error. File may not be found.");
+            }
+        }
+        return currentFile;
+    }
+
+    public static String saveOption(ArrayList<String> list, String currentFile){
+        String fileName = currentFile;
+        JFileChooser jfc = new JFileChooser();
+        if(fileName == null) {
+            Scanner scan = new Scanner(System.in);
+            String prompt = "What is the name of the .txt file that you want to save to?";
+            fileName = SafeInput.getNonZeroLenString(scan, prompt) + ".txt";
+            jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            jfc.showSaveDialog(null); // Makes dialog appear
+            fileName = jfc.getSelectedFile() + "\\" + fileName;
+        }
+
+        try(FileWriter fw = new FileWriter(fileName)){
+            for(int i = 0; i < list.size(); i++){
+                fw.write(list.get(i) + "\n");
+            }
+            currentFile = fileName;
+        }catch(Exception ex){
+            System.out.println("Error. File may not be found.");
+        }
+        return currentFile;
+    }
+
+    public static boolean quitOption(boolean dirty){
+        Scanner scan = new Scanner(System.in);
+        String prompt = "Are you sure you want to quit? (Y/N)";
+        return SafeInput.getYNConfirm(scan, prompt);
+    }
+
+    public static boolean checkForSave(boolean dirty, ArrayList<String> list, String currentFile){
+        if(dirty) {
+            Scanner scan = new Scanner(System.in);
+            String prompt = "Do you want to save your list? Otherwise it will be abandoned. (Y/N)";
+            if(SafeInput.getYNConfirm(scan, prompt)){
+                saveOption(list, currentFile);
+                return false;
+            }
+        }
+        return dirty;
     }
 }
--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,2 +1,173 @@
-package PACKAGE_NAME;public class SafeInput {
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.ArrayList;
+public class SafeInput {
+    /**
+     *
+     * @param pipe a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return a String response that is not zero length
+     */
+    public static String getNonZeroLenString(Scanner pipe, String prompt)
+    {
+        String retString = ""; // Set this to zero length. Loop runs until it isn’t
+        do
+        {
+            System.out.print("\n" +prompt + ": "); // show prompt add space
+            retString = pipe.nextLine();
+        }while(retString.length() == 0);
+        return retString;
+    }
+
+    public static int getInt(Scanner pipe, String prompt)
+    {
+        int retInt = 0;
+        boolean repeat  = true;
+        while (repeat) {
+            try{
+                System.out.print("\n" +prompt + ": ");
+                while (!pipe.hasNext()){
+                    String input = pipe.next();
+                }
+                retInt = pipe.nextInt();
+                repeat = false;
+            }catch (Exception ex){
+                repeat = true;
+            }
+            pipe.nextLine();
+        }
+        return retInt;
+    }
+
+    public static double getDouble(Scanner pipe, String prompt)
+    {
+        double retDouble = 0;
+        boolean repeat  = true;
+        while (repeat) {
+            try{
+                System.out.print("\n" +prompt + ": ");
+                while (!pipe.hasNext()){
+                    String input = pipe.next();
+                }
+                retDouble = pipe.nextDouble();
+                repeat = false;
+            }catch (Exception ex){
+                repeat = true;
+            }
+            pipe.nextLine();
+        }
+        return retDouble;
+    }
+
+    public static int getRangedInt(Scanner pipe, String prompt, int low, int high)
+    {
+
+        int retInt = 0;
+        boolean repeat  = true;
+        while (repeat) {
+            try{
+                System.out.print("\n" +prompt+ "(in range " + low + "-" + high + "): ");
+                while (!pipe.hasNext()){
+                    String input = pipe.next();
+                }
+                retInt = pipe.nextInt();
+                if(retInt >= low && retInt <= high){
+                    repeat = false;
+                }
+            }catch (Exception ex){
+                repeat = true;
+            }
+            pipe.nextLine();
+        }
+        return retInt;
+    }
+
+    public static double getRangedDouble(Scanner pipe, String prompt, double low, double high)
+    {
+        double retDouble = 0;
+        boolean repeat  = true;
+        while (repeat) {
+            try{
+                System.out.print("\n" +prompt+ "(in range " + low + "-" + high + "): ");
+                while (!pipe.hasNext()){
+                    String input = pipe.next();
+                }
+                retDouble = pipe.nextDouble();
+                if(retDouble >= low && retDouble <= high){
+                    repeat = false;
+                }
+            }catch (Exception ex){
+                repeat = true;
+            }
+            pipe.nextLine();
+        }
+        return retDouble;
+    }
+
+    public static boolean getYNConfirm(Scanner pipe, String prompt){
+        String input = "";
+        do{
+            System.out.print("\n" +prompt + ": ");
+            input = pipe.next();
+            if(input.length() == 1){
+                if(input.toLowerCase().equals("y")){
+                    return true;
+                }
+                if(input.toLowerCase() .equals("n")){
+                    return false;
+                }
+            }
+            pipe.nextLine();
+        }while(true);
+    }
+
+    public static String getRegExString(Scanner pipe, String prompt, String regExString){
+        String input = "";
+        Pattern regEx = Pattern.compile(regExString);
+        do{
+            System.out.print("\n" +prompt + " (that matches the following Regex: " + regExString + "): ");
+            input = pipe.next();
+            if(input.matches(regExString)){
+                return input;
+            }
+            pipe.nextLine();
+        }while(true);
+    }
+
+    public static void prettyHeader(String msg){
+        int numPadding = (54 - msg.length()) / 2;
+        for (int i = 0; i < 60; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        System.out.print("***");
+        for (int i = 0; i < numPadding; i++) {
+            System.out.print(" ");
+        }
+        System.out.print(msg);
+        for (int i = 0; i < numPadding; i++) {
+            System.out.print(" ");
+        }
+        if(msg.length() % 2 == 1){
+            System.out.print(" ");
+        }
+        System.out.print("***");
+        System.out.println();
+        for (int i = 0; i < 60; i++) {
+            System.out.print("*");
+        }
+    }
+
+    public static String getValidOption(Scanner pipe, String prompt, ArrayList<String> options){
+        String input = "";
+        do{
+            System.out.print("\n" +prompt + ": ");
+            input = pipe.next();
+            if(options.contains(input)){
+                return input;
+            }
+            pipe.nextLine();
+        }while(true);
+    }
 }
